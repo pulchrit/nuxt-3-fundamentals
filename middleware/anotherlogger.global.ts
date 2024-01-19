69980bed5530f92f5f2fa5353a3a6d509706156f
Melissa Lafranchise <pulchrit@gmail.com>
2024-01-19 14:43:25 -0800

    add inline, named, and global route middleware

---
+++ middleware/anotherlogger.global.ts
@@ -0,0 +1,5 @@
+// because we have global in tthe filename, this middleware will be applied to 
+// all routes!
+export default defineNuxtRouteMiddleware((to, from) => {
+  console.log(to, from, 'from the named middelware');
+})
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,3 +1,23 @@
+
+<script setup>
+  
+  definePageMeta({
+    // One way to add route middleware for only this page!!
+    // you could also use the middleware folder for more global middleware!!
+    // using nuxi cli: npx nuxi add middleware [enter middleware filename]
+    // the middlware runs on the server and client
+    // middleware(to, from) { // use this function to add your own for this page
+    //   // to = route navigate to
+    //   // from = route navigated from 
+    //   console.log(to, from);
+    // },
+    
+    // use the middleware prop to tell Nuxt which middleware from the middleware
+    // folder you want to fun for this page
+    middleware: ["logger"],
+  })
+</script>
+
 <template>
   <MovieSearch />
 </template>
