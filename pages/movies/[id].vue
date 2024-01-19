bc67841aa9dc00b1995c41579f4b451430571905
Melissa Lafranchise <pulchrit@gmail.com>
2024-01-19 07:11:14 -0800

    setup, nav, and routing basics

---
+++ pages/movies/[id].vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+  const route = useRoute()
+</script>
+
+<template>
+  <h1>{{ $route.params.id }}</h1>
+  <h2>{{ route.params.id }}</h2>
+</template>
+
+<style scoped></style>
