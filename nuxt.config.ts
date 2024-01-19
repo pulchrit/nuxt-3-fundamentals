ef566a0bc1d5a9372ae9490dee058e5f064a6c36
Melissa Lafranchise <pulchrit@gmail.com>
2024-01-19 10:56:24 -0800

    use google fonts, install sass, create stylesheets, fetch data from omdb api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiKey: 'daf20600',
+    }
+  },
+  css: ['~/assets/main.scss'],
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Mulish: [400, 900],
+    }
+  }
 })
